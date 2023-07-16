37fe4be81da6abce07b9bcabfde087f2e924eecd
Yunho <yhjc2692@gmail.com>
2023-07-16 15:22:53 -0400

    Refactor (2/3)

--- server/server.py
+++ server/server.py
@@ -1,37 +1,27 @@
-import argparse
+# basics
 import asyncio
 import json
-import logging
 import os
-import ssl
 import uuid
-
-import cv2
+from pathlib import Path
+import time
+import logging
+# aiortc
 from aiohttp import web
 from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
 from av import VideoFrame
-
-import time
-
+# computer vision
+import cv2
+# local modules
 from tracks import VideoTransformTrack
 
 
-from pathlib import Path
-
-ROOT = os.path.dirname(__file__) # NOTE doesn't work in Jupyter
-# ROOT = Path().resolve()
-ROOT
-
-
-
+ROOT = os.path.dirname(__file__)
 
 logger = logging.getLogger("pc")
 pcs = set()
 relay = MediaRelay()
-
-
-
 
 
 async def index(request):
@@ -59,10 +49,10 @@ async def offer(request):
 
     # prepare local media
     # player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
-    if args.record_to:
-        recorder = MediaRecorder(args.record_to)
-    else:
-        recorder = MediaBlackhole()
+    # if args.record_to:
+    #     recorder = MediaRecorder(args.record_to)
+    # else:
+    #     recorder = MediaBlackhole()
 
     @pc.on("datachannel")
     def on_datachannel(channel):
@@ -84,24 +74,25 @@ async def offer(request):
 
         if track.kind == "audio":
             # pc.addTrack(player.audio)
-            recorder.addTrack(track)
+            # recorder.addTrack(track)
+            pass
         elif track.kind == "video":
             pc.addTrack(
                 VideoTransformTrack(
                     relay.subscribe(track), transform=params["video_transform"]
                 )
             )
-            if args.record_to:
-                recorder.addTrack(relay.subscribe(track))
+            # if args.record_to:
+            #     recorder.addTrack(relay.subscribe(track))
 
         @track.on("ended")
         async def on_ended():
             log_info("Track %s ended", track.kind)
-            await recorder.stop()
+            # await recorder.stop()
 
     # handle offer
     await pc.setRemoteDescription(offer)
-    await recorder.start()
+    # await recorder.start()
 
     # send answer
     answer = await pc.createAnswer()
@@ -120,58 +111,4 @@ async def on_shutdown(app):
     coros = [pc.close() for pc in pcs]
     await asyncio.gather(*coros)
     pcs.clear()
-
-
-# parser = argparse.ArgumentParser(
-#     description="WebRTC audio / video / data-channels demo"
-# )
-# parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-# parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-# parser.add_argument(
-#     "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-# )
-# parser.add_argument(
-#     "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
-# )
-# parser.add_argument("--record-to", help="Write received media to a file."),
-# parser.add_argument("--verbose", "-v", action="count")
-# args = parser.parse_args()
-
-
-class Args:
-    cert_file = None  # replace with your SSL certificate file, if any
-    key_file = None  # replace with your SSL key file, if any
-    host = "0.0.0.0"
-    port = 8080
-    record_to = None  # replace with the file path to record to, if any
-    verbose = None  # replace with the verbosity level, if any
-
-args = Args()
-
-
-if __name__ == "__main__":
-
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
-
-    if args.cert_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(args.cert_file, args.key_file)
-    else:
-        ssl_context = None
-
-    app = web.Application()
-    app.on_shutdown.append(on_shutdown)
-    app.router.add_get("/", index)
-    app.router.add_get("/client.js", javascript)
-    app.router.add_post("/offer", offer)
-    web.run_app(
-        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
-    )
-
-
-
-
 
